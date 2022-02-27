93dbedbe9fbd7fc5993a82913afdca3eca5d8796
kkmtyyz <45735463+kkmtyyz@users.noreply.github.com>
2022-02-27 23:17:39 +0900

    Fix RV64A instruction (WIP)

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1464,11 +1464,27 @@ impl Cpu {
     /// x[rd] = AMO64(M[x[rs1]] SWAP x[rs2])
     fn amoswap_d(&mut self, inst: &Instruction) {
         let addr = self.reg.get_reg(inst.rs1);
-        let addr = self.trans_addr(addr);
+        let mut addr = self.trans_addr(addr);
         self.check_pmp(addr, PMPPerm::W);
 
-        let data = self.bus.ld_dram(addr);
-        self.bus.sd_dram(addr, self.reg.get_reg(inst.rs2));
+        let mm = if (addr as usize) < MEM_OFF {
+            true
+        } else {
+            false
+        };
+        let data: u64;
+        if mm {
+            data = self.l_mm(addr as u64);
+        } else {
+            addr = (addr as i64 - MEM_OFF as i64) as u64;
+            data = self.bus.ld_dram(addr);
+        }
+
+        if mm {
+            self.bus.s_mm(addr, self.reg.get_reg(inst.rs2));
+        } else {
+            self.bus.sd_dram(addr, self.reg.get_reg(inst.rs2));
+        }
         self.reg.set_reg(inst.rs2, data);
         self.reg.set_reg(inst.rd, data);
     }
