48d306b41e53bbfb5e1d1889651f0e36faf7dfa0
kkmtyyz <45735463+kkmtyyz@users.noreply.github.com>
2022-03-06 23:59:12 +0900

    Fix RV64A amomax.d instruction

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1616,16 +1616,29 @@ impl Cpu {
     /// x[rd] = AMO64(M[x[rs1]] MAX x[rs2])
     fn amomax_d(&mut self, inst: &Instruction) {
         let addr = self.reg.get_reg(inst.rs1);
-        let addr = self.trans_addr(addr);
+        let mut addr = self.trans_addr(addr);
         self.check_pmp(addr, PMPPerm::W);
 
-        let data = self.bus.ld_dram(addr);
-        self.reg.set_reg(inst.rd, data);
-        if (data as i64) < (self.reg.get_reg(inst.rs2) as i64) {
+        let mm = if (addr as usize) < MEM_OFF {
+            true
+        } else {
+            false
+        };
+        let data: u64;
+        if mm {
+            data = self.l_mm(addr as u64);
+        } else {
+            addr = (addr as i64 - MEM_OFF as i64) as u64;
+            data = self.bus.ld_dram(addr as u64);
+        }
+
+        let rs2_v = self.reg.get_reg(inst.rs2) as i64;
+        if (data as i64) < rs2_v {
             self.bus.sd_dram(addr, self.reg.get_reg(inst.rs2));
         } else {
             self.bus.sd_dram(addr, data);
         }
+        self.reg.set_reg(inst.rd, data);
     }
 
     /// x[rd] = AMO64(M[x[rs1]] MINU x[rs2])
